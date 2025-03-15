03a09d8f8ab160d9ca34212e9e61c9e99a95f056
Minje Sung <minje813@dgu.ac.kr>
2025-03-15 23:43:33 +0900

    feat: 되돌리기 버튼 구현

--- script.js
+++ script.js
@@ -2,21 +2,22 @@ const planet = document.querySelector('.planet');
 const timelineSlider = document.querySelector('input[id="timeline"]');
 const playButton = document.querySelector('button[id="play"]');
 const stopButton = document.querySelector('button[id="stop"]');
+const reverseButton = document.querySelector('button[id="reverse"]');
 
 const planetAnimation = planet.animate(
   [
     { transform: 'translateX(-360px) rotateZ(0deg)' },
-    { transform: 'translateX(360px) rotateZ(360deg)' },
+    { transform: 'translateX(360px) rotateZ(1turn)' },
   ],
   {
     duration: 3000,
-    iterations: Infinity,
     fill: 'both',
   }
 );
 
 playButton.addEventListener('click', () => planetAnimation.play());
 stopButton.addEventListener('click', () => planetAnimation.pause());
+reverseButton.addEventListener('click', () => planetAnimation.reverse());
 timelineSlider.addEventListener('input', (e) => {
   planetAnimation.pause();
   const value = e.target.value;
